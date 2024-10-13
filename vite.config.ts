5b2f48b2dae88a94e835849f9f537d8e46a07f69
egg <robin.eggestig@gmail.com>
2024-10-13 17:32:11 +0000

    test

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
   ],
   base: '/exec-tutorial-test/',
   build: {
-    outDir: 'dist', // Specify the output directory for your build
+    outDir: 'build', // Specify the output directory for your build
   },
-
 });
